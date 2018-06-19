97f5ca701e32b03564d7b974d015b79f44a7863e
Luciano S. Junior <luciano.ssj@hotmail.com>
2018-06-19 15:07:08 -0300

    Commit #03 - Finalização da APS 3 / APS 3 finalization

--- APS3_BD2.sql
+++ APS3_BD2.sql
@@ -1,54 +1,43 @@
 /*.:: APS 3 - Triggers ::.*/
 
+-- Criação da tabela de logs --
 CREATE TABLE logConsulta (
 
     idLog SERIAL PRIMARY KEY,
     operacao TEXT,
-    idPessoa INTEGER,
+    usuario TEXT,
     dataLog TIMESTAMP,
     idConsulta INTEGER,
     datacons DATE,
-    horacons DATE,
-    idOldLog NUMERIC
+    horacons TIME
 
 );
 
-/*
-
-I = INSERT salvar um registro com Valores NEW data e Hora e código da consulta
-
-U = Update salvar dois registros com Valores NEW e OLD data e Hora e código da consulta
-
-D= Delete salvar um registro com Valores OLD data e Hora e código da consulta
-
-*/
-
+-- Criação da função --
 CREATE OR REPLACE FUNCTION registraLogConsulta ()
     RETURNS TRIGGER AS $triggerConsulta$
         BEGIN
 
             IF(TG_OP = 'INSERT') THEN
                 
-                INSERT INTO logConsulta (operacao, idpessoa, dataLog, idConsulta, datacons, horacons)
-                    VALUES ('Agendamento de consulta', new.idpessoa, NOW(), new.idConsulta, 
+                INSERT INTO logConsulta (operacao, usuario, dataLog, idConsulta, datacons, horacons)
+                    VALUES ('Agendamento de consulta', USER, NOW(), new.idConsulta, 
                     new.datacons, new.hora);    
                 
                 RETURN NEW;
 
             ELSEIF (TG_OP = 'UPDATE') THEN
 
-                UPDATE logConsulta 
-                    SET operacao = 'Consulta alterada' WHERE idLog = old.idLog;
-
-                INSERT INTO logConsulta (operacao, idpessoa, dataLog, idConsulta, datacons, horacons, idOldLog)
-                    VALUES ('Alteração de consulta', old.idpessoa, NOW(), old.idConsulta, 
-                    new.datacons, new.hora, old.idLog);
+                INSERT INTO logConsulta (operacao, usuario, dataLog, idConsulta, datacons, horacons)
+                    VALUES ('Alteração de consulta', USER, NOW(), old.idConsulta, 
+                    new.datacons, new.hora);
 
                 RETURN NEW;
 
             ELSEIF (TG_OP = 'DELETE') THEN
 
-                DELETE FROM logConsulta WHERE idLog = old.idLog;
+                INSERT INTO logConsulta (operacao, usuario, dataLog, idConsulta) 
+                	VALUES ('Remoção de consulta', USER, NOW(), old.idConsulta);
 
                 RETURN OLD;
 
@@ -59,5 +48,18 @@ CREATE OR REPLACE FUNCTION registraLogConsulta ()
         END;
 $triggerConsulta$ LANGUAGE plpgsql;
 
+-- Criação do Trigger --
 CREATE TRIGGER triggerConsulta AFTER INSERT OR UPDATE OR DELETE ON Consulta
     FOR EACH ROW EXECUTE PROCEDURE registraLogConsulta();
+       
+-- Testes do Trigger --
+INSERT INTO Consulta (idanimal, idpessoa, hora, datacons, obs) VALUES (1,1,'09:30','2018-06-23','Machucado');
+UPDATE Consulta SET idanimal = '2' WHERE idConsulta = '8';
+DELETE FROM Consulta WHERE idConsulta = 8;
+
+SELECT * FROM Consulta;
+SELECT * FROM logConsulta;
+
+/*DROP TABLE logConsulta;
+DROP TRIGGER triggerConsulta ON Consulta;
+DROP FUNCTION registraLogConsulta;*/
